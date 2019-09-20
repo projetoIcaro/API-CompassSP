5042072a6412361af92d75e8433e9d701eaef8f6
fialhoFabio <fabiom456@gmail.com>
2019-09-20 02:36:42 -0300

    feeeeeeetch

--- server.js
+++ server.js
@@ -14,19 +14,20 @@ app.use(session({
     saveUninitialized: true
 }))
 
+var corsOptions = {
+    "credentials": true,
+    "origin": "http://localhost:3052",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json())
 app.use(express.urlencoded( { extended: true}));
 app.use(morgan('dev'));
-app.use(cors());
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3052'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+app.use(cors(corsOptions));
 
 app.use("/api", require('./src/routes'))
 app.use("/", require('./src/Site/routes'))
